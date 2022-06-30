fb3aeb06dffc04eb44ce3efd3b08099e4c8d882f
Doctor-TVER <ilyinya@yandex.ru>
2022-06-30 08:39:21 +0300

    add code

--- parser.py
+++ parser.py
@@ -1,54 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 import pprint
+import json
 
 domain = 'https://pythonworld.ru'
 url = f'{domain}/bookshop'
 
 response = requests.get(url)
 
-
 # print(response.status_code)
 
 # print(response.text)
 
-# Создаем суп для разбора html
 soup = BeautifulSoup(response.text, 'html.parser')
 
-result = {}
+names = []
 booknames_a = soup.find_all('div', class_='bookname')
-for one_booknames_a in booknames_a[:1]:
+for one_booknames_a in booknames_a[:5]:
     text = one_booknames_a.text
-
-    # print(text, title)
     text1 = text.replace('\n','')
+    names.append(text1.strip())
+
 bookauthors_a = soup.find_all('div', class_='bookauthor')
 titles = []
-for bookauthor_a in bookauthors_a[:1]:
+for bookauthor_a in bookauthors_a[:5]:
     print(bookauthor_a.text)
-titles.append(bookauthor_a.text)
-#
-#     # добавим в словарь
-result[text1] = titles
-#
+    titles.append(bookauthor_a.text)
+
+    # добавим в словарь
+result = dict(zip(names,titles))
+
 pprint.pprint(result)
-# Поиск по классу - Обычно самое полезное!!!
 
-# big_body_div = soup.find('div', class_='modulebody1')
-
-# print(big_body_div)
-# print(type(big_body_div))
-# print(big_body_div.get('class'))
-
-# 1. Лучший вариант искать тожде по классу
-# Можно искать в уже найденном
-# modulebody3 = big_body_div.find('div', class_='modulebody3')
-# print(modulebody3)
-
-# 2. Если не получается найти по классу или по тегу, то придется по порядку
-# .contents
-
-# print(big_body_div.contents)
-# print(len(big_body_div.contents))
-# находим ссылку a по порядку
-# print(big_body_div.contents[1].contents[1].contents)
+with open("result.json", "w", encoding='utf-8') as parser_file:
+    json.dump(result, parser_file)
